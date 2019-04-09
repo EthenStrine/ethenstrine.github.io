d3b25cd2a972d86e8d25b2c033a8ee293912a7f1
Ethen <techdude169@gmail.com>
2019-04-09 09:26:57 -0400

    ,

--- quoteme/javascript.js
+++ quoteme/javascript.js
@@ -9,6 +9,7 @@ var quotes = [
     'Beauty is in the eye of the beholder and it may be necessary from time to time to give a stupid or misinformed beholder a black eye.',
     "Saying 'I notice you're a nerd' is like saying, 'Hey, I notice that you'd rather be intelligent than be stupid, that you'd rather be thoughtful than be vapid, that you believe that there are things that matter more than the arrest record of Lindsay Lohan. Why is that?' In fact, it seems to me that most contemporary insults are pretty lame. Even 'lame' is kind of lame. Saying 'You're lame' is like saying 'You walk with a limp.' Yeah, whatever, so does 50 Cent, and he's done all right for himself.",
     'The story so far\: <b>In the beginning the Universe was created\.</b> This has made a lot of people very angry and been widely regarded as a bad move\.',
+    'The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.',
 ];
 
 function newQuote() {
